2bcfe8e4f1bd989d8a48b0ae88719011dbea23f4
Quentin Rault <q.rault054@gmail.com>
2022-09-27 12:26:14 +0200

    Add camera mode

--- screens/CameraScreen.js
+++ screens/CameraScreen.js
@@ -1,19 +1,51 @@
-import { Button, Text, View, StyleSheet } from 'react-native';
+import { Camera, CameraType } from 'expo-camera';
+import { useState, useEffect } from 'react';
+import { Text, View, TouchableOpacity, StyleSheet } from 'react-native';
 
 export default CameraScreen = () => {
+  const [type, setType] = useState(CameraType.back);
+  const [hasPermission, setHasPermission] = useState(null);
+  function toggleCameraType() {
+    setType((current) =>
+      current === CameraType.back ? CameraType.front : CameraType.back
+    );
+  }
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Camera Screen</Text>
-      <Button title="hey click me"></Button>
-    </View>
+    <Camera style={styles.camera} type={type}>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.button} onPress={toggleCameraType}>
+        </TouchableOpacity>
+      </View>
+    </Camera>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
+  camera: {
     flex: 1,
-    backgroundColor: '#ddd',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  buttonContainer: {
+    flex: 1,
+  },
+  button: {
+    flex: 1,
+  },
+  text: {
+    flex: 1,
   },
 });
